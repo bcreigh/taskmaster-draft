4c47f63a22758d3cfcc934f60edc767765984756
Bob Creigh <bcreigh@bobcreigh.com>
2018-05-28 17:03:41 -0400

    Added Task Component

---
+++ src/components/Task.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Task = (props) => {
+  return (
+    <div>
+      <h3>{props.task}</h3>
+    </div>
+  )
+}
+
+export default Task;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,18 +1,19 @@
 import React, {Component} from 'react';
 
+import Task from './Task';
+
 class Tasks extends Component {
   state = {}
   render() {
     return (
       <div>
-        <div>Tasks Header</div>
-        <div>Task 1</div>
-        <div>Task 2</div>
-        <div>Task 3</div>
+        <h2>Task Header</h2>
+        <Task task='Task 1'/>
+        <Task task='Task 2'/>
+        <Task task='Task 3'/>
       </div>
     )
   }
 }
-//test
 
 export default Tasks;
