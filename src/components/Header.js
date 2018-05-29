c1224972ce25b53f8ea5c24e165f51f87719312c
Bob Creigh <bcreigh@bobcreigh.com>
2018-05-29 19:49:12 -0400

    Added Material-ui to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import AppBar from "@material-ui/core/AppBar";
+import Icon from "@material-ui/core/Icon";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 const Header = () => {
-  return <h1>TaskMaster</h1>
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Icon>
+            assignment_turned_in
+          </Icon>
+          <Typography variant="title" color="inherit">
+            TaskMaster
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
 }
 
 export default Header;
