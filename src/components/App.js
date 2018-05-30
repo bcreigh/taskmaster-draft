b3959e0b61b8c13a5ea4c89a0d3c5a3917b7fb63
Bob Creigh <bcreigh@bobcreigh.com>
2018-05-29 20:35:20 -0400

    Added MuiTheme support

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
 import React, {Component} from 'react';
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import './App.css';
 import Header from './Header';
 import Tasks from './Tasks';
 
 class App extends Component {
   render() {
+    const theme = createMuiTheme({
+      palette: {
+        primary: {
+          light: '#52c7b8',
+          main: '#009688',
+          dark: '#00675b',
+          contrastText: '#fff'
+        },
+        secondary: {
+          light: '#bef67a',
+          main: '#8bc34a',
+          dark: '#5a9216',
+          contrastText: '#000'
+        }
+      }
+    });
     return (
       <div className="App">
-        <Header/>
-        <Tasks/>
+        <MuiThemeProvider theme={theme}>
+          <Header/>
+          <Tasks/>
+        </MuiThemeProvider>
       </div>
     )
   }
