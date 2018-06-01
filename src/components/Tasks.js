1e32a322671ba4f998d3ffffb628ed8c4f9d97f8
Bob Creigh <bcreigh@bobcreigh.com>
2018-06-01 11:58:33 -0400

    added task mapping

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,14 +3,42 @@ import React, {Component} from 'react';
 import Task from './Task';
 
 class Tasks extends Component {
-  state = {}
+  state = {
+    tasks: [
+      {
+        title: "Task 1",
+        description: "This is a description of task number 1",
+        start: new Date("2018-5-1"),
+        due: new Date("2018-6-1"),
+        done: null,
+        complete: false
+      }, {
+        title: "Task 2",
+        description: "This is a description of task number 2",
+        start: new Date("2018-5-2"),
+        due: new Date("2018-6-2"),
+        done: new Date("2018-5-5"),
+        complete: true
+      }, {
+        title: "Task 3",
+        description: "This is a description of task number 3",
+        start: new Date("2018-5-3"),
+        due: new Date("2018-6-3"),
+        done: null,
+        complete: false
+      }
+    ]
+  }
   render() {
     return (
       <div>
         <h2>Task Header</h2>
-        <Task task='Task 1'/>
-        <Task task='Task 2'/>
-        <Task task='Task 3'/>
+        {this
+          .state
+          .tasks
+          .map(task => (< Task task = {
+            task.title
+          } />))}
       </div>
     )
   }
